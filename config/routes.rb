f3694c4fa55b21e704b35a58428269c2b5310124
SaberMyKing <229844109@qq.com>
2019-02-17 21:54:02 +0800

    add devise to model admin_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'admin_users#home'
 end
